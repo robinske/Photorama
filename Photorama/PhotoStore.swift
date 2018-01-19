7bc4c23ce11b8eea74df0174cc83bea3be26b498
Kelley Robinson <krobinson@twilio.com>
2018-01-18 17:36:01 -0800

    Add core data

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -16,6 +16,8 @@ enum PhotosResult {
 
 class PhotoStore {
     
+    let imageStore = ImageStore()
+    
     private let session: URLSession = {
         let config = URLSessionConfiguration.default
         return URLSession(configuration: config)
@@ -44,6 +46,13 @@ class PhotoStore {
     }
     
     func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+        let photoKey = photo.photoID
+        if let image = imageStore.image(forKey: photoKey) {
+            OperationQueue.main.addOperation {
+                completion(.success(image))
+            }
+            return
+        }
         let photoURL = photo.remoteURL
         let request = URLRequest(url: photoURL)
         
@@ -51,6 +60,11 @@ class PhotoStore {
             (data, response, error) -> Void in
             
             let result = self.processImageRequest(data: data, error: error)
+            
+            if case let .success(image) = result {
+                self.imageStore.setImage(image, forKey: photoKey)
+            }
+            
             OperationQueue.main.addOperation {
                 completion(result)
             }
--- PhotosViewController.swift
+++ PhotosViewController.swift
@@ -48,4 +48,19 @@ class PhotosViewController: UIViewController, UICollectionViewDelegate {
         fetchPhotos()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "showPhoto"?:
+            if let selectedIndexPath = collectionView.indexPathsForSelectedItems?.first {
+                let photo = photoDataSource.photos[selectedIndexPath.row]
+                
+                let destinationVC = segue.destination as! PhotoInfoViewController
+                destinationVC.photo = photo
+                destinationVC.store = store
+            }
+        default:
+            preconditionFailure("Unexpected segue identifier.")
+        }
+    }
+    
 }
