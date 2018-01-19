9ad4b241013a0807d87ab8831cdc91df136e63f5
Kelley Robinson <krobinson@twilio.com>
2018-01-18 16:06:48 -0800

    Completed chapter 20 with next and random photo

--- PhotosViewController.swift
+++ PhotosViewController.swift
@@ -1,9 +1,42 @@
-//
-//  PhotosViewController.swift
-//  Photorama
-//
-//  Created by Kelley Robinson on 1/18/18.
-//  Copyright © 2018 krobs. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    var store: PhotoStore!
+    @IBAction func nextImage(_ sender: UIButton) {
+        fetchRandomPhoto()
+    }
+    
+    func updateImageView(for photo: Photo) {
+        store.fetchImage(for: photo) { (imageResult) -> Void in
+            switch imageResult {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error downloading image: \(error)")
+            }
+        }
+    }
+    
+    private func fetchRandomPhoto() {
+        store.fetchInterestingPhotos { (photosResult) -> Void in
+            switch photosResult {
+            case let .success(photos):
+                print("Successfully found \(photos.count) photos.")
+                let randomIndex = Int(arc4random_uniform(UInt32(photos.count)))
+                
+                self.updateImageView(for: photos[randomIndex])
+            case let .failure(error):
+                print("Error fetching interesting photos: \(error)")
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        fetchRandomPhoto()
+    }
+    
+}
